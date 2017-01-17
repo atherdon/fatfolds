95b31154a0b6440fd7220b2b642c305536d73dc8
Constantine <business@netweightit.com>
2017-01-17 09:06:00 -0800

    forms

--- myapp/models/nine/food.js
+++ myapp/models/nine/food.js
@@ -6,10 +6,20 @@
 var mongoose = require('mongoose');
 
 // Define food schema
-module.exports = mongoose.model('Food',{
-	name: String,
+module.exports = mongoose.model('Food', {
+	meal    : String,
+	food    : String,
 	calories: Number,
-	protein: Number,
-	fat: Number,
-	carbs: Number
+	fat     : Number,
+	cholesterol: Number,
+	sodium  : Number,
+	carbs   : Number,
+	protein : Number,
+	sugar   : Number,
+	fiber   : Number,
+	
+	
+
+
+
 });
